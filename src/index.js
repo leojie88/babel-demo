1e1de08098ceb0e96055e910846a08ddb0ec6847
baoguo.wang <baoguo.wang@qunar.com>
2016-07-18 14:42:32 +0800

    add

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import 'babel-polyfill';
+console.log(Array.from('aaa'));
+const sum = (a,b) => (a+b);
+
+let x = (1,2);
+let y = (4,5);
+console.log(x);
---
+++ src/jsx.js
@@ -0,0 +1,7 @@
+import React,{Component} from 'react';
+
+class demoComponent extends Component{
+    render(){
+        return <h1>Hello World</h1>
+    }
+}
